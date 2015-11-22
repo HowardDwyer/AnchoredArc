b27a4289ca233fdbd9d89caaad7c1213616a7c6b
Howard Dwyer <dwyerhoward@gmail.com>
2015-11-22 13:46:33 -0600

    start hull branch

--- My Projects/VS C#/Hull Analysis C Sharp/hull02/hull02/hullUtil.cs
+++ My Projects/VS C#/Hull Analysis C Sharp/hull02/hull02/hullUtil.cs
@@ -2,7 +2,7 @@
     using System.Collections.Generic;
     using System.Linq;
     using System.Text;
-
+    // =======================================================================================
     namespace hull02
     {
         static class HullUtil
