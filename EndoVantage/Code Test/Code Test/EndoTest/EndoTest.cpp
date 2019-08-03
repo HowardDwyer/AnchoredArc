1e352f516a225f04a13b99991fa339c6ec07aa3b
Howard Dwyer <dwyerhoward@gmail.com>
2019-08-03 16:19:54 -0400

    EndoTestBranch: work in SetupModel

--- EndoVantage/Code Test/Code Test/EndoTest/EndoTest.cpp
+++ EndoVantage/Code Test/Code Test/EndoTest/EndoTest.cpp
@@ -24,42 +24,58 @@ void AllocateArrays();
 void PopulateArrays();
 void MatrixMatrix();
 void MatrixVector();
-void setupModel();
+void SetupModel();
 
 double* X;
 double* Y;
 double** area;
 double** ATA;
-double DX_GLOBAL = 10;
+double DX_GLOBAL = 0.5;
 
 int main(int argc, char** argv)
 {
 	// Setup Model
-	setupModel();
+	cout << "SetupModel\n";
+	SetupModel();
 
-	// Allocate Arrays
+/*	// Allocate Arrays
+	cout << "AllocateArrays\n";
 	AllocateArrays();
 
 	// POPULATE AREA AND X ARRAYS
+	cout << "PopulateArrays\n";
 	PopulateArrays();
 
 	// MATRIX MULTIPLICATION
+	cout << "MatrixMatrix\n";
 	MatrixMatrix();
 
 	// COMPUTE Y = ATA*X
+	cout << "MatrixVector\n";
 	MatrixVector();
 
 	// PRINT L2 NORM OF Y
 	double norm = 0;
+	cout << "Y=( ";
 	for (int i = 0; i < 3 * m_nNumNodes; i++)
 	{
+		cout << Y[i] << " ";
 		norm = norm + Y[i] * Y[i];
 	}
+	cout << ")\n";
+	cout << "Squared norm = "<<norm<<'\n';
 	norm = sqrt(norm);
-	clock_t total = clock();
-	printf("(%3.5lf) L2 Norm\n", norm);
-	printf("(%3.5lf seconds) in Total\n", ((float)total) / CLOCKS_PER_SEC);
+	cout<< norm <<" L2 Norm\n";
+*/
+	//----------------------------------------------------------------------------------------------
+	clock_t total = clock();  // total execution time,in ticks
+	cout << "printf stmts\n";
+//	printf("(%3.5lf) L2 Norm\n", norm);
+//	printf("(%3.5lf seconds) in Total\n", ((float)total) / CLOCKS_PER_SEC);
+	double totalTime = ((double)total) / CLOCKS_PER_SEC; // dwyerdebug
+	cout << totalTime << " seconds in Total\n";          // dwyerdebug
 
+	cout << "return\n";
 	return 0;
 }
 
@@ -138,52 +154,72 @@ void PopulateArrays()
 		}
 	}
 }
-void setupModel() {
 
+//=========== SET UP MODEL ==================================================================
+// width and height are hard-coded as 200 x 200
+void SetupModel() {
 	int nnode = 0;
 	int nelem = 0;
+
+	// a 1D array of doubles which store 3D coord X,Y,Z for each node
 	double* nodecoord1;
+
+	// vertex indices: each element is composed of 3 nodes
 	int* conn1;
+
 	int ny = 0;
 	int nx = 0;
 
 	double dx = DX_GLOBAL;
 	double width = 200;
 	double height = 200;
-	nx = (int)(width / dx + 1);
-	ny = (int)(height / dx + 1);
-	nnode = nx * ny;
-	nodecoord1 = new double[3 * nnode];
-	nelem = (nx - 1) * (ny - 1) * 2;
-	conn1 = new int[3 * nelem];
+	nx = (int)(width / dx + 1);  // number of columns of nodes
+	ny = (int)(height / dx + 1); // number of rows of nodes
+	nnode = nx * ny;             // total number of nodes
 
+	nodecoord1 = new double[3 * nnode]; // (X,Y,Z) for the coordinates
+	nelem = (nx - 1) * (ny - 1) * 2; // number of triangle elements
+	conn1 = new int[3 * nelem];      // node indices for the elements
+
+	//----------------------------------------------------------------------------------
+	double nodeXValue;
+	double negHalfWidth = -width * 0.5;
+	double negHalfHeight = -height * 0.5;
+
+	//----------------------------------------------------------------------------------
 	// create simple mesh
-	int cnt = 0;
+	int cnt;
+	int threeCnt = 0;
 	for (int i = 0; i < nx; i++) {
+		nodeXValue = negHalfWidth + i * dx;
 		for (int j = 0; j < ny; j++) {
-			nodecoord1[3 * cnt + 0] = -width / 2.0 + i * dx;
-			nodecoord1[3 * cnt + 1] = -height / 2.0 + j * dx;
-			nodecoord1[3 * cnt + 2] = 0;
-			cnt++;
+			nodecoord1[threeCnt] = nodeXValue;
+			nodecoord1[threeCnt + 1] = negHalfHeight + j * dx;
+			nodecoord1[threeCnt + 2] = 0;
+			threeCnt += 3;
 		}
 	}
-	cnt = 0;
+
+	//----------------------------------------------------------------------------------
+//	cnt = 0;
+	threeCnt = 0;
 	for (int i = 0; i < nx - 1; i++) {
 		for (int j = 0; j < ny - 1; j++) {
-			conn1[3 * cnt] = i * ny + j + 1;
-			conn1[3 * cnt + 1] = (i + 1) * ny + j + 1;
-			conn1[3 * cnt + 2] = i * ny + j + 1 + 1;
-			cnt++;
+			conn1[threeCnt] = i * ny + j + 1;
+			conn1[threeCnt + 1] = (i + 1) * ny + j + 1;
+			conn1[threeCnt + 2] = i * ny + j + 1 + 1;
+//			cnt++;
+			threeCnt += 3;
 
-			conn1[3 * cnt] = (i + 1) * ny + j + 1;
-			conn1[3 * cnt + 1] = (i + 1) * ny + j + 1 + 1;
-			conn1[3 * cnt + 2] = i * ny + j + 1 + 1;
-			cnt++;
-
+			conn1[threeCnt] = (i + 1) * ny + j + 1;
+			conn1[threeCnt + 1] = (i + 1) * ny + j + 1 + 1;
+			conn1[threeCnt + 2] = i * ny + j + 1 + 1;
+//			cnt++;
+			threeCnt += 3;
 		}
 	}
 
-
+	//----------------------------------------------------------------------------------
 	m_nNumNodes = nnode;
 	m_nNumElems = nelem;
 
@@ -192,21 +228,29 @@ void setupModel() {
 	m_pNodes = new CNode[m_nNumNodes];
 	m_pElements = new CElement[m_nNumElems];
 
+	//----------------------------------------------------------------------------------
+	int threeI=0;
 	for (int i = 0; i < m_nNumNodes; i++) {
-
-		m_pNodes[i].SetX(nodecoord1[3 * i]);
-		m_pNodes[i].SetY(nodecoord1[3 * i + 1]);
-		m_pNodes[i].SetZ(nodecoord1[3 * i + 2]);
+		m_pNodes[i].SetX(nodecoord1[threeI]);
+		m_pNodes[i].SetY(nodecoord1[threeI + 1]);
+		m_pNodes[i].SetZ(nodecoord1[threeI + 2]);
 		m_pNodes[i].SetLabel(i + 1);
 		m_pNodes[i].SetInternalLabel(i);
+		threeI += 3;
 	}
 
+	//----------------------------------------------------------------------------------
 	// Setup elements
+	threeI = 0;
 	for (int i = 0; i < m_nNumElems; i++) {
-		m_pElements[i].SetNode(0, &m_pNodes[conn1[3 * i] - 1]);
-		m_pElements[i].SetNode(1, &m_pNodes[conn1[3 * i + 1] - 1]);
-		m_pElements[i].SetNode(2, &m_pNodes[conn1[3 * i + 2] - 1]);
+		m_pElements[i].SetNode(0, &m_pNodes[conn1[threeI] - 1]);
+		m_pElements[i].SetNode(1, &m_pNodes[conn1[threeI + 1] - 1]);
+		m_pElements[i].SetNode(2, &m_pNodes[conn1[threeI + 2] - 1]);
 		m_pElements[i].SetLabel(i + 1);
+		threeI += 3;
 	}
-
-}
+	//------------------------------------------------------------------------------------
+	// release resources
+	delete[] nodecoord1;
+	delete[] conn1;
+}// SetupModel
